32cf200dec9aa337f070d436981662060b755c71
김인영 <>
2024-07-19 16:26:03 +0900

    Resolve merge conflicts

--- chatapp-client/src/socket.js
+++ chatapp-client/src/socket.js
@@ -1,3 +1,6 @@
-import { io } from "socket.io-client";
-const socket = io("http://localhost:5001");
+// socket.js
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:5000'); // 서버 URL과 포트가 올바른지 확인
+
 export default socket;
